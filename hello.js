f36692f490591e7abceeab1f7dbfedec3fe0e0eb
Abhishek Chaubey <abhishekchaubey1999@gmail.com>
2024-03-30 21:00:55 +0530

    modules in Nodejs

---
+++ hello.js
@@ -0,0 +1,5 @@
+const math = require('./math');
+
+console.log("Addition is: " + math.add(3, 5))
+console.log("Subtraction is: " + math.subtract(3, 5))
+console.log("Multiplication is: " + math.multiply(3, 5))
