72f0a9764051264b86e2f918df257cee42b3836b
Abhishek Chaubey <abhishekchaubey1999@gmail.com>
2024-03-30 22:25:08 +0530

    http server

--- hello.js
+++
@@ -1,5 +0,0 @@
-const math = require('./math');
-
-console.log("Addition is: " + math.add(3, 5))
-console.log("Subtraction is: " + math.subtract(3, 5))
-console.log("Multiplication is: " + math.multiply(3, 5))
