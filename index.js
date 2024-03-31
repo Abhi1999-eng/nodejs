4e0087dc3184c99757c3daacc17fccea65030fae
Abhishek Chaubey <abhishekchaubey1999@gmail.com>
2024-03-31 21:31:35 +0530

    expressjs

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express()
 app.get('/', (req, res) => {
 	res.send('hello world')
 })
+app.get('/about', (req, res) => {
+	res.send('this is about page')
+})
+
 
 app.listen(port, () => {
-	console.log(`Server started on port ${port}`)
+	console.log(`Server started on port` + 3000)
 })
