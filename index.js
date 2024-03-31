fce534322359d8d5ee1f1f35e761dc28684ba2d1
Abhishek Chaubey <abhishekchaubey1999@gmail.com>
2024-03-31 21:04:43 +0530

    till expressjs"

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
-const http = require('http')
-const fs = require('fs')
-const url = require('url')
+const express = require('express')
+const port = 3000
 
-const myServer = http.createServer((req, res) => {
+const app = express()
 
-	const myUrl = url.parse(req.url, true)
-	console.log(myUrl.query.name);
-	// Log the incoming request
-	const log = `New request received on : ${new Date()} at ${myUrl.pathname}\n`
-
-	// Append the log to the file
-	fs.appendFile('log.txt', log, err => {
-		if (err) {
-			console.error('Error writing to log file:', err)
-		} else {
-			switch (myUrl.pathname) {
-				case '/':
-					res.end('This is home page')
-					break;
-
-				case '/about':
-					res.end('This is about page')
-					break;
-
-				default:
-					res.end(`This is ${myUrl.pathname} page`)
-					break;
-			}
-		}
-	})
-
+app.get('/', (req, res) => {
+	res.send('hello world')
 })
 
-myServer.listen(3000, () => {
-	console.log('Server is running on port 3000.....')
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`)
 })
