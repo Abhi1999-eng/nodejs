f794a810aa15daedd4583077ce34cb1739c86854
Abhishek Chaubey <abhishekchaubey1999@gmail.com>
2024-04-01 15:48:42 +0530

    mongo

--- index.js
+++ index.js
@@ -1,61 +1,102 @@
 const express = require('express')
-const users = require('./MOCK_DATA.json')
+const mongoose = require('mongoose')
 const fs = require('fs')
-const { log } = require('console')
 const port = 8000
 
+mongoose.connect('mongodb://localhost:27017/nodejs', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}).then(() => {
+	console.log('Connected to MongoDB')
+}).catch((err) => {
+	console.log(err)
+})
+
+const userSchema = new mongoose.Schema({
+	first_name: String,
+	last_name: String,
+	email: {
+		type: String,
+		unique: true
+	},
+	gender: String,
+	job_title: String
+})
+
+const User = mongoose.model('User', userSchema)
 const app = express()
 app.use(express.json({ extended: false }))
 
-//Routes
-app.get('/api/users', function (req, res) {
-	return res.json(users)
+// Routes
+// Get all users
+app.get('/api/users', async (req, res) => {
+	try {
+		const users = await User.find()
+		res.json(users)
+	} catch (err) {
+		console.error(err.message)
+		res.status(500).send('Server Error')
+	}
 })
 
-app.get('/users', function (req, res) {
-	const html = `<ul>${users.map(user => `<li>${user.first_name} ${user.last_name}</li>`).join('')}</ul>`
-	res.send(html)
+// Get user by ID
+app.get('/api/users/:id', async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id)
+		if (!user) {
+			return res.status(404).json({ msg: 'User not found' })
+		}
+		res.json(user)
+	} catch (err) {
+		console.error(err.message)
+		if (err.kind === 'ObjectId') {
+			return res.status(404).json({ msg: 'User not found' })
+		}
+		res.status(500).send('Server Error')
+	}
 })
 
-app.get('/api/users/:id', function (req, res) {
-	var id = req.params.id
-	id = parseInt(id);
-	const user = users.find(user => user.id === id);
-	return res.json(user)
+// Create a user
+app.post('/api/users', async (req, res) => {
+	try {
+		const newUser = new User(req.body)
+		await newUser.save()
+		res.json(newUser)
+	} catch (err) {
+		console.error(err.message)
+		res.status(500).send('Server Error')
+	}
 })
 
-app.post('/api/users', function (req, res) {
-	var body = req.body
-	users.push({
-		id: users.length + 1,
-		...body
-	})
-	fs.writeFileSync('./MOCK_DATA.json', JSON.stringify(users))
-	return res.json(body)
-
-});
-
-app.patch('/api/users/:id', function (req, res) {
-	var id = req.params.id
-	id = parseInt(id);
-	const user = users.find(user => user.id === id);
-	user.first_name = req.body.first_name ? req.body.first_name : user.first_name
-	user.last_name = req.body.last_name ? req.body.last_name : user.last_name
-	user.email = req.body.email ? req.body.email : user.email
-	user.gender = req.body.gender ? req.body.gender : user.gender
-	user.job_title = req.body.job_title ? req.body.job_title : user.job_title;
-	fs.writeFileSync('./MOCK_DATA.json', JSON.stringify(users))
-	return res.json(user)
+// Update a user
+app.patch('/api/users/:id', async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id)
+		if (!user) {
+			return res.status(404).json({ msg: 'User not found' })
+		}
+		user.set(req.body)
+		await user.save()
+		res.json(user)
+	} catch (err) {
+		console.error(err.message)
+		res.status(500).send('Server Error')
+	}
 })
 
-app.delete('/api/users/:id', function (req, res) {
-	var id = req.params.id
-	id = parseInt(id);
-	const user = users.find(user => user.id === id);
-	const index = users.indexOf(user);
-	users.splice(index, 1);
-	fs.writeFileSync('./MOCK_DATA.json', JSON.stringify(users))
-	return res.json(user)
+// Delete a user
+app.delete('/api/users/:id', async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id)
+		if (!user) {
+			return res.status(404).json({ msg: 'User not found' })
+		}
+		await user.remove()
+		res.json({ msg: 'User removed' })
+	} catch (err) {
+		console.error(err.message)
+		res.status(500).send('Server Error')
+	}
 })
 
 app.get('/', function (req, res) {
